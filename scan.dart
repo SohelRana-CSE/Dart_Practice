edab28693f0e27ba3bcc721d47ccb1d98d78e74d
Dart_Basic_Practice <soheldcc18@gmail.com>
2024-02-12 22:06:44 +0600

    Dart Basic Code

---
+++ scan.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+
+  print("Please Enter Your Name: ");
+  String? name = stdin.readLineSync();
+  print('Your Name is $name');
+
+  print('Enter your father name:');
+  String? fName = stdin.readLineSync();
+  print("Your Father Name is $fName");
+
+
+  print('Enter your age:');
+  int age = int.parse(stdin.readLineSync());
+  print('Your age is $age');
+}
